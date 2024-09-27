6936d0bff55aa692d26dd12b7a769efabbfb21c6
ogrigriii <57384448+Coachsteveee@users.noreply.github.com>
2024-09-27 21:55:15 +0200

    update for GW6

--- wsh.py
+++ wsh.py
@@ -8,15 +8,15 @@ def load_data():
         A pandas dataframe containing the GW4 data.
     """
 
-    df = pd.read_excel("GW5.xlsx").drop(columns=['Unnamed: 0'])
+    df = pd.read_excel("GW6.xlsx").drop(columns=['Unnamed: 0'])
     return df
 
 def main():
-    """Loads the GameWeek 4 data from an excel file and displays it
+    """Loads the GameWeek 6 data from an excel file and displays it
     in a Streamlit dataframe. The dataframe is displayed with its
     index hidden and a fixed height of 900px, and it is allowed
     to use the full width of the Streamlit app."""
-    st.title('Game Week 5')
+    st.title('Game Week 6')
     
     data = load_data()
     st.dataframe(data, hide_index=True, height=560, use_container_width=True)
